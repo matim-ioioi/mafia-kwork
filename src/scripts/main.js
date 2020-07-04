e871126a0d803e9c053d0dbf9fea30054a7c9fc6
Tim <timamavrin@gmail.com>
2020-07-04 14:00:53 +0700

    Add the project

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('.nav-slider').width($('.active').width());
+  $('.nav-slider').css('left', $('.active').outerWidth(true)/2-$('.nav-slider').outerWidth(true)/2);
+  $('.nav-slider').css('top', $('.active').outerHeight(true));
+
+  $('.nav-menu').hover(function (){
+    var offsetItemLeft = $(this).position().left;
+    var offsetItemTop = $(this).position().top;
+    $('.nav-slider').css('left', offsetItemLeft+$(this).outerWidth(true)/2-$('.nav-slider').outerWidth(true)/2);
+    $('.nav-slider').css('top', offsetItemTop+$(this).outerHeight(true));
+  });
+  $('.nav-menu').mouseout(function () { 
+    $('.nav-slider').css('left', $('.active').outerWidth(true)/2-$('.nav-slider').outerWidth(true)/2);
+    $('.nav-slider').css('top', $('.active').outerHeight(true));
+  });
+});
