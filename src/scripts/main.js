b27a8b9476c77b97decf23bdcc27f703a6b7fc5d
Tim <timamavrin@gmail.com>
2020-07-04 19:00:58 +0700

    some fixes and add a friends div

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // nav-slider init
   $(".nav-slider").width($(".active").width());
   $(".nav-slider").css(
     "left",
@@ -6,6 +7,7 @@ $(document).ready(function () {
   );
   $(".nav-slider").css("top", $(".active").outerHeight(true));
 
+  // handling navs (highlight etc)
   $(".nav-menu").hover(function () {
     var offsetItemLeft = $(this).position().left;
     var offsetItemTop = $(this).position().top;
@@ -24,4 +26,27 @@ $(document).ready(function () {
     );
     $(".nav-slider").css("top", $(".active").outerHeight(true));
   });
+
+  // friends div init
+  $("#friends-div").css(
+    "max-height",
+    $(document).innerHeight() -
+      $("#profile-div").outerHeight(true) -
+      $(".nav-pan").outerHeight(true) -
+      $(".friends-pan").outerHeight(true) -
+      $("#add-friend-div").outerHeight(true) +
+      "px"
+  );
+  // friends div loading/creating
+  for (let i = 0; i < 6; i++) {
+    $("#friends-div").append(
+      "<div class='border mb-3 friends' id='friend_" +
+        i +
+        "'><img class='rounded-circle fr-img' src='imgs/default.jpg' alt='photo' width='" +
+        ($(".add-friend").innerWidth() / 4 - 20) +
+        "px' height='" +
+        ($(".add-friend").innerHeight() - 20) +
+        "px'/></div>"
+    );
+  }
 });
